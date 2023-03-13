b888a28c1e3680d1747521c962e3c0a02c341786
Nays <123313530+Naystie@users.noreply.github.com>
2023-03-13 04:29:43 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,39 @@
+## Discord Message Sender
+This Python script allows you to send messages to a Discord channel at specified intervals.
+
+
+# Requirements
+Python 3.x
+requests library
+colorama library
+
+# Installation
+**1.** Clone this repository to your computer or download it as a ZIP file.
+**2.** Install the requirements by running the following commands:
+
+```py
+
+pip install requests
+pip install colorama
+
+```
+**3.** Create a file named token.txt and save your Discord bot's access token in this file.
+
+# Usage
+**1.** Run the script in the command prompt:
+
+```py
+
+py mes.py
+
+```
+or run **run.bat** (for windows)
+
+**2.** Enter the channel ID, the message to send, and the message sending interval (in seconds).
+**3.** The script will send the message at the specified intervals. Press CTRL+C to stop the process.
+
+# Note
+This script may exceed Discord API limits and lead to the ban of your Discord account or bot. Before using this script, read the Discord API rules and limitations and try to stay under these limits.
+
+# License
+This project is licensed under the MIT License.
---
+++ mes.py
@@ -0,0 +1,61 @@
+import requests
+import time
+import os
+from colorama import init, Fore, Style
+
+init(convert=True)
+
+
+class Exploit:
+    def __init__(self, token, channel_id, message):
+        self.token = token
+        self.channel_id = channel_id
+        self.message = message
+        self.headers = {'Authorization': token}
+
+    def send_message(self):
+        """Send the message."""
+        res = requests.post(
+            f'https://discordapp.com/api/v6/channels/{self.channel_id}/messages',
+            headers=self.headers,
+            json={'content': self.message}
+        )
+
+        if res.status_code == 200:
+            print(f'{Fore.GREEN}Message sent successfully: {self.message}{Style.RESET_ALL}')
+        else:
+            print(f'{Fore.RED}Error sending message: {res.content.decode()}{Style.RESET_ALL}')
+
+
+def main():
+    print(f'{Fore.BLUE}Welcome to the Discord Message Sender!{Style.RESET_ALL}\n')
+
+    token_file = 'token.txt'
+
+    if not os.path.isfile(token_file):
+        print(f'{Fore.RED}Error: {token_file} file not found.{Style.RESET_ALL}\n')
+        time.sleep(5)
+        return
+
+    with open(token_file, 'r') as f:
+        token = f.readline().strip()
+
+    channel_id = input('Enter channel ID: ')
+    message = input('Enter message to send: ')
+    interval = int(input('Enter message interval (in seconds): '))
+
+    print(f'\n{Fore.BLUE}Sending message every {interval} seconds...{Style.RESET_ALL}\n')
+
+    exploit = Exploit(token, channel_id, message)
+
+    # Send the initial message
+    exploit.send_message()
+
+    # Loop and send the message every interval seconds
+    while True:
+        time.sleep(interval)
+        exploit.send_message()
+
+
+if __name__ == '__main__':
+    main()
